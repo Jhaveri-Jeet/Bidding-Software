f8793244f451ddd6a07385bd8cd2be3b58ad1ea5
JeetJhaveri <jeetwork941@gmail.com>
2023-11-12 15:01:00 +0530

    readme file

--- assets/includes/init.php
+++ assets/includes/init.php
@@ -1,7 +1,7 @@
 <?php
 
-define("BASE_DIR", $_SERVER['DOCUMENT_ROOT'] . "/Biding");
-define("BASE_URL", "/Biding");
+define("BASE_DIR", $_SERVER['DOCUMENT_ROOT'] . "/Bidding");
+define("BASE_URL", "/Bidding");
 
 date_default_timezone_set('Asia/Kolkata');
 
